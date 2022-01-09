af9503a6b135e37ee2cf4a84b3058a03ce88dab9
miguel <mfpbbr@gmail.com>
2022-01-08 23:02:59 -0300

    CHANGE BASICS

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+    <div>
+      <main>
+        <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <div className={styles.grid}>
-          <a href="/signin" className={styles.card}>
+        <div>
+          <a href="/signin">
             <h2>SIGN IN</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="/signup" className={styles.card}>
+          <a href="/signup">
             <h2>SIGN UP</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
